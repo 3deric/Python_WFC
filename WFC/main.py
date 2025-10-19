5a94f193335fd1532f1c5b1764b7d36d8c12bec3
Eric <eric.schubert.3d@gmail.com>
2025-10-19 11:29:44 +0200

    Added reset functionality

--- WFC/main.py
+++ WFC/main.py
@@ -3,14 +3,14 @@ from wavefunctioncollapse import WaveFunctionCollapse as WFC
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((640, 640))
+screen = pygame.display.set_mode((960, 960))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Wave Function Collapse")
 running = True
 dt = 0
 
 
-wfc = WFC(10, screen)
+wfc = WFC(15, screen)
 
 while running:
     for event in pygame.event.get():
@@ -19,6 +19,8 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 wfc.set_collapse()
+            if event.key == pygame.K_r:
+                wfc.set_reset()
 
     screen.fill("black")
     wfc.auto_collapse()
--- WFC/wavefunctioncollapse.py
+++ WFC/wavefunctioncollapse.py
@@ -8,11 +8,11 @@ ELEMENTS = [
             WS(0,'img/tile_0000.png', [[5,13,14,15],[1,2,11],[7,11,13],[2,5,9,15]]),
             WS(1,'img/tile_0001.png', [[5,13.14,15],[1,2,11],[8,12,14],[0,1,10]]),
             WS(2,'img/tile_0002.png', [[5,14,15],[0,5,7,13],[9,10,15],[0,1,10]]),
-            WS(3,'img/tile_0003.png', [[6,8,12],[14,15],[9,10,15],[6,7,12,8]]),
-            WS(4,'img/tile_0004.png', [[6,8,12],[6,8,12],[7,11,13],[3,13,14]]),
+            WS(3,'img/tile_0003.png', [[6,8,12],[4,14,15],[9,10,15],[6,7,12,8]]),
+            WS(4,'img/tile_0004.png', [[6,8,12],[6,8,9,12],[7,11,13],[3,13,14]]),
             WS(5,'img/tile_0005.png', [[5,13,14,15],[0,5,7,13],[0,1,2,5],[2,5,9,15]]),
             WS(6,'img/tile_0006.png', [[1,6,8,10,11,12],[3,6,8,10,12],[3,4,6,8,12,14],[6,8,12]]),
-            WS(7,'img/tile_0007.png', [[0,4,7],[3,6,8,10,12,13],[7,11,13],[2,5,9,15]]),
+            WS(7,'img/tile_0007.png', [[0,4,7],[3,6,8,9,10,12,13],[7,11,13],[2,5,9,15]]),
             WS(8,'img/tile_0008.png', [[1,6,8,10,11,12],[6,8,9,10,12],[4,6,8,12,14],[4,6,8,7,11,12]]),
             WS(9,'img/tile_0009.png', [[2,3,9],[0,5,7,13],[9,10,15],[4,6,7,8,11]]),
             WS(10,'img/tile_0010.png', [[2,3,9],[1,2,11],[6,8,12,14],[6,8,9,11,12]]),
@@ -34,14 +34,18 @@ class WaveFunctionCollapse:
         self.auto_collapse_wait = 50
         self.screen = screen
 
-        self.setup_world_elements()
-        self.set_world_element_neighbours()
+        self.set_reset()
 
     def set_collapse(self):
         self.auto_collapse_active =  bool(1 - int(self.auto_collapse_active))
 
+    def set_reset(self):
+        self.auto_collapse_active = False
+        self.setup_world_elements()
+        self.set_world_element_neighbours()
+
     def setup_world_elements(self):
-        print("setup!")
+        self.world_elements = []
         for y in range(self.world_size):
             for x in range(self.world_size):
                 we = WE(self.screen, ELEMENTS, x, y)
