508b3d4a2457ed69be10d56aa078d562541ba955
Steve Mayne <steve.mayne@gmail.com>
2025-10-20 10:31:51 +0100

    Use tile pixels rather than compatability matrix

--- WFC/worldelement.py
+++ WFC/worldelement.py
@@ -18,35 +18,38 @@ class WorldElement:
     def draw(self):
         i = 0
         for element in self.elements:
-            if element == None:
+            if not element:
                 return
             element.draw(self.screen, len(self.elements), i, self.collapsed, self.pos)
             i+=1
 
     def collapse(self):
-        if self.collapsed == True:
+        if self.collapsed:
             return
         self.collapsed = True
         self.elements = [self.elements[random.randrange(0, len(self.elements))]]
         self.entropy = 9999
         for i in range(4):
             temp = []
-            if self.neighbours[i] != None:
+            if self.neighbours[i]:
                 for element in self.neighbours[i].elements:
-                    if element == None or self.elements[0] == None:
+                    if not (element and self.elements[0]):
                         return
-                    if element.id in self.elements[0].neighbours[i]:
+                    if self._is_compatible(self.elements[0], element, i):
                         temp.append(element)
-                if self.neighbours[i].collapsed == False:
+                if not self.neighbours[i].collapsed:
                     if len(temp) == 0:
                         temp.append(None)
                     self.neighbours[i].elements = temp
                     self.neighbours[i].update()
 
     def update(self):
-        if self.collapsed == False:
+        if not self.collapsed:
             self.entropy = len(self.elements)
         if self.entropy == 0:
             print(f"Sprite {self.pos} has zero entropy, consider checking neighbour assignments!")
 
-
+    def _is_compatible(self, src_tile, neighbor_tile, dir_index):
+        allowed = src_tile.compat.get(dir_index)
+        if isinstance(allowed, (list, set)):
+            return neighbor_tile.id in allowed
--- WFC/worldsprite.py
+++ WFC/worldsprite.py
@@ -6,17 +6,31 @@ BORDER = 0.2
 
 class WorldSprite:
 
-    def __init__(self,id, image, neighbours):
+    def __init__(self, id, image, edges=None):
         self.id = id
         self.image = pygame.image.load(image)
-        self.neighbours = neighbours
-        
+        # Store corner colors (NW, NE, SE, SW) as raw RGB tuples
+        self.corners = self._corners_from_image(self.image)
+
+    def _corners_from_image(self, surface):
+        # Sample the color at each corner pixel and store as (R,G,B)
+        w, h = surface.get_width(), surface.get_height()
+        coords = {
+            0: (0, 0),            # NW
+            1: (w - 1, 0),        # NE
+            2: (w - 1, h - 1),    # SE
+            3: (0, h - 1),        # SW
+        }
+        def to_rgb(color):
+            return (color[0], color[1], color[2])
+        return {k: to_rgb(surface.get_at(pos)) for k, pos in coords.items()}
+
     def draw(self, screen, count, i, collapsed, pos):
         w = math.ceil(math.sqrt(count))
         x = math.floor(i % w)
         y = math.floor(i / w)
         offset = (0,0)
-        if count == 1 and collapsed == True:
+        if count == 1 and collapsed:
             out_img = pygame.transform.scale(self.image, (SIZE, SIZE))
         else:
             out_img = pygame.transform.scale(self.image, (SIZE / w * (1-BORDER), SIZE / w * (1 - BORDER)))
